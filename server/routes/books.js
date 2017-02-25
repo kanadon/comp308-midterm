9064d52e04effdf80acf478d9e82a65e35b06b87
ddoubovt <300773484@cencol.ca>
2017-02-25 14:56:49 -0500

    - workin

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,26 +25,59 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    res.render('books/details');
+  
+    res.render('books/details', {
+      title: 'Add a book',
+      books: {
+        'Title': '',
+        'Price': '',
+        'Author': '',
+        'Genre': ''
+      }
+    });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
+  let newBook = book({
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    book.create(newBook, (err, book) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        res.redirect('/books');
+      }
+    });
 
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+try {
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+      book.findById(id, (err, books) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+          res.render('books/details', {
+            "Title": "book details"
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/errors/404');
+    }
 });
 
 // POST - process the information passed from the details form and update the document
